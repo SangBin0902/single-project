a0407b4e11f3bead711efbdce088660b321fd754
SangBin0902 <SangBin0902@gmail.com>
2024-10-08 15:31:44 +0900

    first commit

---
+++ src/main/java/com/sangbin/sangbin/SangbinApplication.java
@@ -0,0 +1,13 @@
+package com.sangbin.sangbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangbinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangbin/sangbin/SangbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangbin.sangbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
